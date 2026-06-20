c1f171c4cb2f57fb398f248dad233a1a05c5fee7
kda1404 <darina_kuchumova@mail.ru>
2026-06-20 15:21:33 +0300

    Update solution.c

--- problem1-fork-proc-print-numbers/solution.c
+++ problem1-fork-proc-print-numbers/solution.c
@@ -18,6 +18,30 @@ int main(int argc, char *argv[]) {
 
     int n = atoi(argv[1]);
 
+    for (int i = 1; i <= n; i++) {
+        pid_t pid = fork();
+        
+        if (pid == -1) {
+            perror("fork");
+            return 1;
+        }
+
+        if (pid == 0) {
+            printf("%d", i);
+            if (i < n) {
+                printf(" ");
+            } else {
+                printf("\n");
+            }
+            return 0;
+        } else {
+            wait(NULL);
+            if (i == n) {
+                return 0;
+            }
+        }
+    }
+
     // TODO: создайте цепочку из N процессов (каждый не более чем с одним потомком).
     //       Каждый процесс выводит одно число. Порядок вывода должен быть 1 2 3 ... N.
 
